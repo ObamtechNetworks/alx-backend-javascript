8e9f2669930ddd2c001c18093b0d32c4e41039dd
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-23 02:30:13 +0100

    iterating through objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,22 @@
+export default function createIteratorObject(report) {
+  // Extract all employees from all departments into a single array
+  const allEmployees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees.push(...department);
+  }
+
+  // Return an iterator object
+  return {
+    [Symbol.iterator]() {
+      let index = 0;
+      return {
+        next() {
+          if (index < allEmployees.length) {
+            return { value: allEmployees[index++], done: false };
+          }
+          return { value: undefined, done: true };
+        },
+      };
+    },
+  };
+}
