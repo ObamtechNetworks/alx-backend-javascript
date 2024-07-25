928490b87a77d689d07f5b36d83e4191ae681978
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 02:46:44 +0100

    check parameter types

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+// eslint-disable-next-line consistent-return
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return { status: result.status, value: result.value };
-    }
-    return { status: result.status, value: result.reason };
-  }));
+  if (typeof firstName === 'string' || typeof lastName === 'string' || typeof fileName === 'string') {
+    return Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]).then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    }));
+  }
 }
