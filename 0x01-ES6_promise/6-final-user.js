59e75c4be5bb60f2a080e29aa41d57c68ab023ff
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 04:14:45 +0100

    refactored and used foreach

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
-  const uploadphoto = uploadPhoto(fileName);
   const status = [];
-
-  return Promise.allSettled([signUp, uploadphoto]).then((values) => {
-    values.forEach((value) => {
-      if (value.status === 'fulfilled') {
-        status.push({ status: value.status, value: value.value });
-      } else if (value.status === 'rejected') {
-        status.push({ status: value.status, value: `${value.reason}` });
+  if (typeof firstName === 'string' && typeof lastName === 'string' && typeof fileName === 'string') {
+    return Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]).then((results) => results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        status.push({ status: result.status, value: result.value });
+      } else if (result.status === 'rejected') {
+        status.push({ status: result.status, value: result.reason });
       }
-    });
-    return status;
-  });
+    }));
+  }
+  return status;
 }
