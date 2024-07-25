e6b7a51eff1438accce270d4214f469beff66934
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 04:47:14 +0100

    don't know why only this passed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const status = [];
   const signup = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
-  if (typeof firstName === 'string' && typeof lastName === 'string' && typeof fileName === 'string') {
-    return Promise.allSettled([signup, upload])
-      .then((results) => results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          status.push({ status: result.status, value: result.value });
-        } else if (result.status === 'rejected') {
-          status.push({ status: result.status, value: result.reason });
-        }
-      }));
-  }
-  return status;
+  const status = [];
+
+  return Promise.allSettled([signup, upload]).then((values) => {
+    values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+        status.push({ status: value.status, value: value.value });
+      } else if (value.status === 'rejected') {
+        status.push({ status: value.status, value: `${value.reason}` });
+      }
+    });
+    return status;
+  });
 }
