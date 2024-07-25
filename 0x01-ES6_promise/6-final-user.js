501ef6d17a28aee87fb2162deb284f8860ee6573
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 04:41:52 +0100

    ensured all the returned promise are called

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,17 +3,17 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const status = [];
+  const signup = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
   if (typeof firstName === 'string' && typeof lastName === 'string' && typeof fileName === 'string') {
-    return Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]).then((results) => results.forEach((result) => {
-      if (result.status === 'fulfilled') {
-        status.push({ status: result.status, value: result.value });
-      } else if (result.status === 'rejected') {
-        status.push({ status: result.status, value: result.reason });
-      }
-    }));
+    return Promise.allSettled([signup, upload])
+      .then((results) => results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          status.push({ status: result.status, value: result.value });
+        } else if (result.status === 'rejected') {
+          status.push({ status: result.status, value: result.reason });
+        }
+      }));
   }
   return status;
 }
