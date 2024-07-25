9327394c824e718685dab2dac9cdc306bfe9748a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 02:37:15 +0100

    return promise directly

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  // Uaw Promse.allSettled to handle both promises
-  const results = await Promise.allSettled([
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
-
-  // Map the results to the required format
-  return results.map((result) => {
+  ]).then((results) => results.map((result) => {
     if (result.status === 'fulfilled') {
       return { status: result.status, value: result.value };
     }
-    return { value: result.reason };
-  });
+    return { status: result.status, value: result.reason };
+  }));
 }
