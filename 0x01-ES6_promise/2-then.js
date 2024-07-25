c5a570cd1b81b1ce4a22f40fba53293a61b4edec
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 01:07:16 +0100

    logs to concsole for every resolution

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({
-    status: 200,
-    body: 'success',
-  })).catch((error) => {
-    throw error;
-  }).finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    }).catch(() => new Error());
 }
