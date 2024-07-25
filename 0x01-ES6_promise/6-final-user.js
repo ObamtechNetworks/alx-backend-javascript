356a9e35fbeba02279a569e8eb59d3578edb8a4c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 04:01:42 +0100

    hope this works

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  if (typeof firstName === 'string' && typeof lastName === 'string' && typeof fileName === 'string') {
-    return Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]).then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
+  const signUp = signUpUser(firstName, lastName);
+  const uploadphoto = uploadPhoto(fileName);
+  const status = [];
+
+  return Promise.allSettled([signUp, uploadphoto]).then((values) => {
+    values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+        status.push({ status: value.status, value: value.value });
+      } else if (value.status === 'rejected') {
+        status.push({ status: value.status, value: `${value.reason}` });
       }
-      return { status: result.status, value: result.reason };
-    }));
-  }
-  // Return an empty array or an appropriate response when input validation fails
-  return Promise.resolve([]);
+    });
+    return status;
+  });
 }
