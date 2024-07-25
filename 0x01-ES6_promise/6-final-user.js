19d168d21f2ee3602aeda6fcac97b65bb54a0baf
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-25 01:59:56 +0100

    handle multiple promises when settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Uaw Promse.allSettled to handle both promises
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  // Map the results to the required format
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: result.status, value: result.value };
+    }
+    return { status: result.status, value: result.reason };
+  });
+}
