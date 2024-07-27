a0f0a3e3e4e1ee3e2a28c9cc5214b2df4156114d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-27 05:59:36 +0100

    Ev car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  /* eslint-disable no-underscore-dangle */
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color); // Call parent constructor
+    this._range = range;
+  }
+
+  // Override the cloneCar method to return an instance of Car
+  cloneCar() {
+    // Create a new Car instance and copy properties from this EVCar instance
+    const clone = new Car(this._brand, this._motor, this._color);
+    return clone;
+  }
+
+  // Getter for range
+  get range() {
+    return this._range;
+  }
+}
