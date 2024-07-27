c430f91d24052947bb38726f95f6b3f15d96fe75
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-27 01:00:39 +0100

    building, abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,9 @@
+export default class Building {
+  constructor(sqft) {
+    /* eslint-disable no-underscore-dangle */
+    this._sqft = sqft;
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
