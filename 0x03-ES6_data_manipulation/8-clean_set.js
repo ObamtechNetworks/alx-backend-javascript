6540b7e685e741048ad5681428085aa50c351389
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-31 04:28:14 +0100

    edge cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,25 @@
 export default function cleanSet(set, startString) {
-// using the startswith method that works on strings
+  // If startString is empty, return an empty string
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
 
-  // using the forEach method on the set, check if all starts with the startstirng
-  // create an array to hold the strings
+  // Create an array to hold the strings
   const strArr = [];
-  // create a variable to hold the length of the startString,
-  // so as to take the remaining parts of the string that we check for using substring method
+
+  // Get the length of the startString
   const startStringLen = startString.length;
+
+  // Iterate through each element in the set
   set.forEach((element) => {
-    if (element.startsWith(startString) && startStringLen !== 0) {
+    // Check if the element starts with the startString
+    if (typeof element === 'string' && element.startsWith(startString)) {
+      // Get the rest of the string and push to the array
       const restOfString = element.substring(startStringLen);
       strArr.push(restOfString);
     }
   });
 
-  // join each element in the strArry by dashes '-'
-  const finalString = strArr.join('-');
-  return finalString;
+  // Join each element in the strArr by dashes '-'
+  return strArr.join('-');
 }
