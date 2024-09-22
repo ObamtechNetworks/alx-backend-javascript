672e6cb81091e3c9cbb231e3b42411672e25212f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-22 04:34:21 +0100

    fix eslint errors

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,6 @@
 // full_server/server.js
 import express from 'express';
+// eslint-disable-next-line import/extensions
 import routes from './routes/index.js';
 
 const app = express();
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,13 +1,14 @@
 // full_server/utils.js
 import { readFile } from 'fs/promises';
 
+// eslint-disable-next-line import/prefer-default-export
 export const readDatabase = async (filePath) => {
   try {
     const data = await readFile(filePath, { encoding: 'utf8' });
-    const lines = data.split('\n').filter(line => line.trim().length > 0);
+    const lines = data.split('\n').filter((line) => line.trim().length > 0);
     const result = {};
 
-    lines.forEach(line => {
+    lines.forEach((line) => {
       const [firstName, , , field] = line.split(',');
       if (field) {
         if (!result[field]) result[field] = [];
