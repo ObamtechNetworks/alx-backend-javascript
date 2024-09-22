92b11b96cfe889fe239652e3b33e9b3b1c29e45a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-22 03:28:00 +0100

    students

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,4 @@
 const http = require('http');
-// eslint-disable-next-line no-unused-vars
 const fs = require('fs').promises;
 
 async function countStudents(path) {
@@ -9,18 +8,12 @@ async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
     if (data) {
-      // split the file content into lines
       const lines = data.split('\n');
-
-      // skip the header line and process each subsequent line
       // eslint-disable-next-line no-plusplus
       for (let i = 1; i < lines.length; i++) {
         const line = lines[i].trim();
         if (line) {
-          // split the line by comma
           const [firstName, lastName, age, field] = line.split(',');
-
-          // push the student data (each student object) into results
           results.push({
             firstName: firstName.trim(),
             lastName: lastName.trim(),
@@ -28,7 +21,6 @@ async function countStudents(path) {
             field: field.trim(),
           });
 
-          // count students in each field
           if (fieldCount[field]) {
             fieldCount[field].count += 1;
             fieldCount[field].students.push(firstName.trim());
@@ -42,15 +34,12 @@ async function countStudents(path) {
       }
     }
 
-    // Total number of students
     const totalStudents = results.length;
     let output = `Number of students: ${totalStudents}\n`;
-
-    // print number of studetns in each field
     for (const [field, { count, students }] of Object.entries(fieldCount)) {
       output += `Number of students in ${field}: ${count}. List: ${students.join(', ')}\n`;
     }
-    return output; // Return the formatted string
+    return output;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
@@ -58,31 +47,28 @@ async function countStudents(path) {
 
 const hostname = 'localhost';
 const port = 1245;
-
-// get database file
 const databaseFile = process.argv[2];
 
-// create http server
 const app = http.createServer(async (req, res) => {
-  // handle routin based on the request URL
   if (req.url === '/') {
-    // set the response http header with the http status and content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     try {
-      const studentList = await countStudents(databaseFile);
+      const studentList = (await countStudents(databaseFile)).trim('\n');
       res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(`This is the list of our students\n${studentList.trim('\n')}`);
+      res.end(`This is the list of our students\n${studentList}`);
     } catch (error) {
+    //   console.error(error); // Log the error for debugging
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Cannot load the database');
     }
   } else {
-    //
+    // res.writeHead(404, { 'Content-Type': 'text/plain' });
+    // res.end('Endpoint not found');
   }
 });
 
-// make the server listen on teh specified port
 app.listen(port, hostname);
 
 module.exports = app;
