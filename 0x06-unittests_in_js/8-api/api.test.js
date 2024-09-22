7178818a6f4cfcc9114f220ccee2112399cf75eb
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-22 05:36:31 +0100

    tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,7 +6,7 @@ describe('Index page', () => {
   it('should return status 200 and correct message', (done) => {
     request('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
+      chai.expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
