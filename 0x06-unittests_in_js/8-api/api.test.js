ca0887c2c29bdc8daac5fdb8feb5e7907278cdc2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-23 03:50:32 +0100

    fixed bug

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,15 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api'); // Load the app
+const app = require('./api'); // Import the Express app
 
 describe('Index page', () => {
   it('should return status 200 and correct message', (done) => {
     request('http://localhost:7865', (error, response, body) => {
+      // Expect status code to be 200
       expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.equal('Welcome to the payment system');
-      done();
+      // Expect body to match "Welcome to the payment system"
+      expect(body).to.equal('Welcome to the payment system');
+      done(); // Mark test as done
     });
   });
 });
