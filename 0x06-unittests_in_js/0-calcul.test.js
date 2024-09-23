1f6021900dab3110d30bf97848ef58bc175186fa
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-23 03:19:57 +0100

    passed all test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return 4 when adding 1 and 3', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+  it('Rounds and adds two numbers', () => {
+    assert.strictEqual(calculateNumber(2.5, 3.3), 6);
   });
-
-  it('should return 5 when adding 1 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('Round 0 and a positive number', () => {
+    assert.strictEqual(calculateNumber(0, 5), 5);
   });
-
-  it('should return 5 when adding 1.2 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('Round 0 and a negative number', () => {
+    assert.strictEqual(calculateNumber(0, -2), -2);
   });
-
-  it('should return 6 when adding 1.5 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('Round two negative numbers', () => {
+    assert.strictEqual(calculateNumber(-2.4, -3.7), -6);
+  });
+  it('Round one positive and one negative number', () => {
+    assert.strictEqual(calculateNumber(3.2, -1.8), 1);
   });
 });
