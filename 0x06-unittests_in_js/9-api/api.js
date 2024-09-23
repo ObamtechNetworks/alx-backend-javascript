722e13da2bbb519bf6d3f19b59b39eb946f7c1dc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-23 04:23:10 +0100

    added / route

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
 // Define a route for GET /cart/:id
 app.get('/cart/:id(\\d+)', (req, res) => {
   const cartId = req.params.id;
