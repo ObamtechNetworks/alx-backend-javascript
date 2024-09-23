edfd23eaba6249ae37f0f48c1f2eb19fd8bcebef
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-23 04:38:03 +0100

    hope you pass man

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,3 +1,19 @@
+const chai = require('chai');
+const request = require('request');
+
+
+describe('Testing index page', () => {
+  const url = 'http://localhost:7865';
+  it('Checks and test the main page status code and body ', (done) => {
+    request(url, (err, res, body) => {
+      chai.expect(res.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Welcome to the payment system');
+      done();
+    })
+})
+});
+
+
 describe('GET /available_payments', () => {
     it('should return available payment methods', (done) => {
         chai.request(server)
